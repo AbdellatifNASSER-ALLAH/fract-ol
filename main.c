04b8d0e823c7ab0f4309d9c2c3bfde1b5052d831
Abdellatif Nasser-Allah <abdnasse@c3r6p10.1337.ma>
2025-03-10 11:41:08 +0000

    drawed somethig :)

--- main.c
+++ main.c
@@ -6,11 +6,17 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 22:11:17 by abdnasse          #+#    #+#             */
-/*   Updated: 2025/03/10 11:22:28 by abdnasse         ###   ########.fr       */
+/*   Updated: 2025/03/10 11:39:33 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+
+typedef struct	s_point
+{
+	int	x;
+	int y;
+}	t_point;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -18,6 +24,24 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
+}
+
+void	draw_something(t_data **img, int high, int width)
+{
+	t_point	center;
+	int	i;
+
+	center.x = high / 2;
+	center.y = width / 2;
+	i = 0;
+	while (i < high / 2 - 10)
+	{	
+		my_mlx_pixel_put(img, center.x, center.y + i, 345543);
+		my_mlx_pixel_put(img, center.x, center.y - i, 345543);
+		my_mlx_pixel_put(img, center.x + i, center.y, 3465543);
+		my_mlx_pixel_put(img, center.x - i, center.y, 3465543);
+		i++;
+	}
 }
 
 int	main()
@@ -31,10 +55,8 @@ int	main()
 	img.img = mlx_new_image(mlx, 400, 400);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
-	my_mlx_pixel_put(&img, 200, 200, 63464);
-	my_mlx_pixel_put(&img, 201, 200, 63464);
-	my_mlx_pixel_put(&img, 202, 200, 63464);
-	my_mlx_pixel_put(&img, 203, 200, 63464);
+	draw_something(&img, 400, 400);
+
 	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
