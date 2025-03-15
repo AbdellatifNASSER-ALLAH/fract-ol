7442b1dd0b69f98592fb99d56f3fbe9f6fe575f2
Abdellatif Nasser-Allah <abdnasse@c3r4p9.1337.ma>
2025-03-15 21:01:50 +0000

    from scratch !

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 17:17:49 by abdnasse          #+#    #+#             */
-/*   Updated: 2025/03/15 17:18:02 by abdnasse         ###   ########.fr       */
+/*   Updated: 2025/03/15 21:01:38 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
 	*(unsigned int*)dst = color;
 }
